9a5b0187884c93891ffd8fbb00f12f5a91c91fb0
Rajesh S <122197373+rajeshs098@users.noreply.github.com>
2023-06-04 12:58:37 +0530

    Update login.php

--- other/login.php
+++ other/login.php
@@ -8,13 +8,13 @@
 
 </head>
 <!DOCTYPE html>
-<!-- Created by CodingLab |www.youtube.com/c/CodingLabYT-->
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
-    <!--<title> Login and Registration Form in HTML & CSS | CodingLab </title>-->
+   
     <link rel="stylesheet" href="style.css">
-    <!-- Fontawesome CDN Link -->
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
